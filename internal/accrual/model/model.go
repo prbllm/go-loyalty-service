98c7eb0c8d7e17b0ed6c1e6437b1ed5bb0a947a4
Pavel Ershov <polruff@gmail.com>
2025-12-18 16:24:52 +0300

    fix model

--- internal/accrual/model/model.go
+++ internal/accrual/model/model.go
@@ -1,40 +1,39 @@
 package model
 
-// Order — заказ, полученный от доверенного источника
 type Order struct {
 	Number  string      // номер заказа
 	Goods   []Good      // список купленных товаров
 	Status  OrderStatus // статус расчета начисления
-	Accrual *int64      // рассчитанные баллы к начислению, nil = нет начисления
-}
-
-type RegisterOrderRequest struct {
-	Number string `json:"order"` // номер заказа
-	Goods  []struct {
-		Description string  `json:"description"`
-		Price       float64 `json:"price"`
-	} `json:"goods"`
+	Accrual *int64      // рассчитанные баллы к начислению(в копейках), nil = нет начисления
 }
 
 type Good struct {
-	Description string `json:"description"` // наименование товара
-	Price       int64  `json:"price"`       // цена оплаченного товара(в копейках)
-}
-
-type GetOrderResponse struct {
-	Number  string   `json:"order"`             // номер заказа
-	Status  string   `json:"status"`            // статус расчета начисления
-	Accrual *float64 `json:"accrual,omitempty"` // рассчитанные баллы к начислению, nil = нет начисления
+	Description string // наименование товара
+	Price       int64  // цена оплаченного товара(в копейках)
 }
 
 type OrderStatus string
 
 const (
-	Registered OrderStatus = "REGISTERED" // заказ зарегистрирован, но не начисление не рассчитано;
+	Registered OrderStatus = "REGISTERED" // заказ зарегистрирован, но начисление не рассчитано
 	Processing OrderStatus = "PROCESSING" // расчёт начисления в процессе
 	Processed  OrderStatus = "PROCESSED"  // расчёт начисления окончен
-	Invalid    OrderStatus = "INVALID"    // заказ не принят к расчёту, и вознаграждение не будет начислено
+	Invalid    OrderStatus = "INVALID"    // заказ не принят к расчёту и вознаграждение не будет начислено
 )
+
+type RegisterOrderRequest struct {
+	Number string `json:"order"` // номер заказа
+	Goods  []struct {
+		Description string  `json:"description"` // наименование товара
+		Price       float64 `json:"price"`       // цена оплаченного товара(в рублях)
+	} `json:"goods"` // список купленных товаров
+}
+
+type GetOrderResponse struct {
+	Number  string   `json:"order"`             // номер заказа
+	Status  string   `json:"status"`            // статус расчета начисления
+	Accrual *float64 `json:"accrual,omitempty"` // рассчитанные баллы к начислению(в рублях), nil = нет начисления
+}
 
 // RewardRule — правило начисления за товар
 type RewardRule struct {
